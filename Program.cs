d43ada7881c04695f1017bc99be8995bb78f5dc8
Chandrasekhar HD <chandrashekar@gniyes.com>
2025-11-15 14:43:11 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿public class Addition
+{
+    public Addition(int a, int b)
+    {
+        Console.WriteLine($"Result is:{ a + b}");
+    }
+    public static void Main(string[] args)
+    {
+        Addition obj = new Addition(1, 2);
+    }
+}
