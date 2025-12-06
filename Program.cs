110af92c89d6285a035848448ce98faf2954809f
Chandrasekhar HD <chandrashekar@gniyes.com>
2025-12-06 15:25:55 +0530

    connit1

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 ﻿public class Addition
 {
-    public Addition(int a, int b)
-    {
-        Console.WriteLine($"Result is:{ a + b}");
-    }
     public static void Main(string[] args)
     {
-        Addition obj = new Addition(1, 2);
+        Console.WriteLine("Inside chari app /n");
     }
 }
